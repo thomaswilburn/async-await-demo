35ac22ab21f314ed25536165be48176dbd95a108
Thomas Wilburn <tbone@thomaswilburn.net>
2017-03-22 21:05:12 -0700

    Uncomment and cleanup on show notes

--- script.js
+++ script.js
@@ -5,27 +5,28 @@ install.batch("fs", "wait", "util").then(async function([fs, wait, util]) {
   console.log("hello");
   
   // quick intro to promises:
-  // var p = wait.waitPromise(1000);
-  // p.then(() => fs.readFilePromise())
-  //   .then(file => console.log(file));
+  var p = wait.waitPromise(1000);
+  p.then(() => fs.readFilePromise())
+    .then(file => console.log("promise file", file));
   
   // Promise.all() lets us coalesce multiple promises into a single super-promise
-  // var all = Promise.all([
-  //   fs.readFilePromise(),
-  //   new Promise(ok => fs.readFileCallback(ok))
-  // ]);
+  var all = Promise.all([
+    fs.readFilePromise(),
+    new Promise(ok => fs.readFileCallback(ok))
+  ]);
   
-  // all.then(function([a, b]) {
-  //   console.log(a, b);
-  // });
+  all.then(function([a, b]) {
+    console.log("all", a, b);
+  });
   
   // await means that we can skip then() and go straight to the value
-  // var file = await fs.readFilePromise();
+  var file = await fs.readFilePromise();
+  console.log("await for file", file);
   
   // we can also await a Promise.all() wrapper, to fire parallel requests
   // otherwise, multiple lines of `await` will be sequential (i.e., slow)
   try {
-    var [a, b, c] = await Promise.all([
+    var [a, b, c, d] = await Promise.all([
       fs.readFilePromise(),
       fs.readFilePromise(),
       fs.readFilePromise(),
@@ -35,23 +36,18 @@ install.batch("fs", "wait", "util").then(async function([fs, wait, util]) {
     console.error(err);
   }
   
-  console.log(a, b, c, d);
+  console.log("using all with await", a, b, c);
   
   // We can also use await with new browser APIs like fetch().
-  // var [text, second] = await Promise.all([
-  //   fetch("sample.txt").then(p => p.text()),
-  //   fetch("fs.js").then(p => p.text())
-  //   ]);
+  var [text, second] = await Promise.all([
+    fetch("sample.txt").then(p => p.text()),
+    fetch("fs.js").then(p => p.text())
+    ]);
     
-  // console.log(text, second.length);
+  console.log("fetch example", text, second.length);
   
   // finally, I built the module system for this presentation around `async` and `await`.
-  var util = install("util");
-  util.then(u => console.log(u));
-  
-  // random generator example - they were precursor to async functions, used for iteration
-  function* generator(x) {
-    yield x + 1;
-  };
+  // `await install("module")` is basically an async `require("module")` from CommonJS
+  var util = await install("util");
 
 });
